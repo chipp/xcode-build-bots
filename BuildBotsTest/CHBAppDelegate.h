42287c43c6becca86fdb0e6ff25cd104bae922ac
Vladimir Burdukov <chippcheg@gmail.com>
2013-11-02 11:30:42 +0300

    create project

---
+++ BuildBotsTest/CHBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CHBAppDelegate.h
+//  BuildBotsTest
+//
+//  Created by Vladimir Burdukov on 11/1/13.
+//  Copyright (c) 2013 cocoaheads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BuildBotsTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BuildBotsTest
+//
+//  Created by Vladimir Burdukov on 11/1/13.
+//  Copyright (c) 2013 cocoaheads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CHBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CHBAppDelegate class]));
+    }
+}
